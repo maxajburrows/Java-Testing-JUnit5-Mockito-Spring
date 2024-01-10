fd2373d1326b41a12b4c67645f875e2ed08bba9d
Max Burrows <maxajburrows@gmail.com>
2024-01-10 13:06:12 +0100

    Add test for create user method

--- TestLifeCyclePerClassDemo/test/com/appsdeveloperblog/service/UserServiceImplTest.java
+++ TestLifeCyclePerClassDemo/test/com/appsdeveloperblog/service/UserServiceImplTest.java
@@ -1,27 +1,52 @@
 package com.appsdeveloperblog.service;
 
+import com.appsdeveloperblog.io.UsersDatabase;
+import com.appsdeveloperblog.io.UsersDatabaseMapImpl;
 import org.junit.jupiter.api.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class UserServiceImplTest {
 
-    // git test again
+    UsersDatabase usersDatabase;
+    UserService userService;
+
+
     @BeforeAll
     void setup() {
         // Create & initialize database
+        usersDatabase = new UsersDatabaseMapImpl();
+        usersDatabase.init();
+        userService = new UserServiceImpl(usersDatabase);
     }
 
     @AfterAll
     void cleanup() {
         // Close connection
         // Delete database
+        usersDatabase.close();
     }
 
     @Test
     @Order(1)
     @DisplayName("Create User works")
     void testCreateUser_whenProvidedWithValidDetails_returnsUserId() {
+
+        // Arrange
+        Map<String, String> user = new HashMap<>();
+        user.put("firstName", "Sergey");
+        user.put("lastName", "Kargopolov");
+
+        // Act
+        String createdUserId = userService.createUser(user);
+
+        // Assert
+        assertNotNull(createdUserId, "User id should not be null");
 
     }
 
