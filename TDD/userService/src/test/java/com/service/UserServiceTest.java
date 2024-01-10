6d10f703ca3e974258fc5796f1a4eccc5dacc97d
Max Burrows <maxajburrows@gmail.com>
2024-01-10 14:46:12 +0100

    Create UserService interface and class

---
+++ TDD/userService/src/test/java/com/service/UserServiceTest.java
@@ -0,0 +1,17 @@
+package com.service;
+
+import com.service.UserService;
+import org.junit.jupiter.api.Test;
+
+public class UserServiceTest {
+
+    @Test
+    void testCreateUser_whenUserDetailsProvided_returnsUserObject() {
+        // Arrange
+        UserService userService = new UserServiceImpl();
+
+        //Act
+
+        //Assert
+    }
+}
