acbcc782f22e1269351b3cb7b184fed73d140b99
Max Burrows <maxajburrows@gmail.com>
2024-01-10 12:25:46 +0100

    Test github connection

--- TestLifeCyclePerClassDemo/test/com/appsdeveloperblog/service/UserServiceImplTest.java
+++ TestLifeCyclePerClassDemo/test/com/appsdeveloperblog/service/UserServiceImplTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.*;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class UserServiceImplTest {
 
+    // git test
     @BeforeAll
     void setup() {
         // Create & initialize database
