1342bbd445afb6583b57b65e1345ac6f2afcb92f
Max Burrows <maxajburrows@gmail.com>
2024-01-10 13:22:16 +0100

    Complete test lifecycle demo

--- TestLifeCyclePerClassDemo/test/com/appsdeveloperblog/service/UserServiceImplTest.java
+++ TestLifeCyclePerClassDemo/test/com/appsdeveloperblog/service/UserServiceImplTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.*;
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -15,6 +15,7 @@ public class UserServiceImplTest {
 
     UsersDatabase usersDatabase;
     UserService userService;
+    String createdUserId;
 
 
     @BeforeAll
@@ -43,7 +44,7 @@ public class UserServiceImplTest {
         user.put("lastName", "Kargopolov");
 
         // Act
-        String createdUserId = userService.createUser(user);
+        createdUserId = userService.createUser(user);
 
         // Assert
         assertNotNull(createdUserId, "User id should not be null");
@@ -56,6 +57,19 @@ public class UserServiceImplTest {
     @DisplayName("Update user works")
     void testUpdateUser_whenProvidedWithValidDetails_returnsUpdatedUserDetails() {
 
+        // Arrange
+        Map<String, String> newUserDetails = new HashMap<>();
+        newUserDetails.put("firstName", "John");
+        newUserDetails.put("lastName", "Kargopolov");
+
+        // Act
+        Map updatedUserDetails = userService.updateUser(createdUserId, newUserDetails);
+
+        // Assert
+        assertEquals(newUserDetails.get("firstName"), updatedUserDetails.get("firstName"),
+                "Returned value of user's first name is incorrect.");
+        assertEquals(newUserDetails.get("lastName"), updatedUserDetails.get("lastName"),
+                "Returned value of user's last name is incorrect.");
     }
 
     @Test
@@ -63,12 +77,27 @@ public class UserServiceImplTest {
     @DisplayName("Find user works")
     void testGetUserDetails_whenProvidedWithValidUserId_returnsUserDetails() {
 
+        // Act
+        Map userDetails = userService.getUserDetails(createdUserId);
+
+        // Assert
+        assertNotNull(userDetails, "User details should not be null");
+        assertEquals(createdUserId, userDetails.get("userId"),
+                "Returned user details contains incorrect user id");
+
     }
 
     @Test
     @Order(4)
     @DisplayName("Delete user works")
     void testDeleteUser_whenProvidedWithValidUserId_returnsUserDetails() {
+
+        // Act
+        userService.deleteUser(createdUserId);
+
+        // Assert
+        assertNull(userService.getUserDetails(createdUserId),
+                "User should not be found");
 
     }
 
