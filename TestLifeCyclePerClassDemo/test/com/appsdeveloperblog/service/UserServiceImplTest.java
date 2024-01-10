00ed9e54f73ac79ffcbd4117d014962abb653880
Max Burrows <maxajburrows@gmail.com>
2024-01-10 12:34:38 +0100

    Try to fix repo

--- TestLifeCyclePerClassDemo/test/com/appsdeveloperblog/service/UserServiceImplTest.java
+++ TestLifeCyclePerClassDemo/test/com/appsdeveloperblog/service/UserServiceImplTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.*;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class UserServiceImplTest {
 
-    // git test
+    // git test again
     @BeforeAll
     void setup() {
         // Create & initialize database
