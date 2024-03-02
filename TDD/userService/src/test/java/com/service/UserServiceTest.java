4fbeddc52e902c26849fb7d25acc966c061091f3
Max Burrows <maxajburrows@gmail.com>
2024-03-02 19:30:48 +0100

    Call a real method successfully (must mock implementation)

--- TDD/userService/src/test/java/com/service/UserServiceTest.java
+++ TDD/userService/src/test/java/com/service/UserServiceTest.java
@@ -24,7 +24,7 @@ public class UserServiceTest {
     @Mock
     UsersRepository usersRepository;
     @Mock
-    EmailVerificationService emailVerificationService;
+    EmailVerificationServiceImpl emailVerificationService;
 
     String firstName;
     String lastName;
