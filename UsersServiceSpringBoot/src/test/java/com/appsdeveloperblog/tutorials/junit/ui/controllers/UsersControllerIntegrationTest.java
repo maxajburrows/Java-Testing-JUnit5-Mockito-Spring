baf0b9cc4bad98f77cee3b60bc0ad845700bbf82
Max Burrows <maxajburrows@gmail.com>
2024-03-07 10:10:09 +0100

    Define test port number in different ways

--- UsersServiceSpringBoot/src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UsersControllerIntegrationTest.java
+++ UsersServiceSpringBoot/src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UsersControllerIntegrationTest.java
@@ -1,14 +1,21 @@
 package com.appsdeveloperblog.tutorials.junit.ui.controllers;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT,
-properties = {"server.port=8081","hostname=192.168.0.2"})
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+//@TestPropertySource(locations = "/application-test.properties",
+//        properties = "server.port=8081")
+//@TestPropertySource(locations = "/application-test.properties")
 public class UsersControllerIntegrationTest {
+
+    @Value("${server.port}")
+    private int serverPort;
 
     @Test
     void contextLoads() {
-
+        System.out.println("server.port=" + serverPort);
     }
 }
