a971887287b3104a116bc06664fb8aa792d7fa29
Max Burrows <maxajburrows@gmail.com>
2024-03-07 09:50:14 +0100

    Use SpringBootTest annotation

---
+++ UsersServiceSpringBoot/src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UsersControllerIntegrationTest.java
@@ -0,0 +1,7 @@
+package com.appsdeveloperblog.tutorials.junit.ui.controllers;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UsersControllerIntegrationTest {
+}
