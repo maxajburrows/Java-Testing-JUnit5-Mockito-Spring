e514fe766c009d8b72d4ca254c77c72d5f663d58
Max Burrows <maxajburrows@gmail.com>
2024-03-07 10:00:01 +0100

    Start test application on defined port

--- UsersServiceSpringBoot/src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UsersControllerIntegrationTest.java
+++ UsersServiceSpringBoot/src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UsersControllerIntegrationTest.java
@@ -1,7 +1,14 @@
 package com.appsdeveloperblog.tutorials.junit.ui.controllers;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT,
+properties = {"server.port=8081","hostname=192.168.0.2"})
 public class UsersControllerIntegrationTest {
+
+    @Test
+    void contextLoads() {
+
+    }
 }
