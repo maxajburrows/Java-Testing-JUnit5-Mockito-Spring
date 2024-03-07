8ae66bc5e5c754e03dc89856d931aefe6fc67f35
Max Burrows <maxajburrows@gmail.com>
2024-03-07 10:16:31 +0100

    Use a random port for test web environment

--- UsersServiceSpringBoot/src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UsersControllerIntegrationTest.java
+++ UsersServiceSpringBoot/src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UsersControllerIntegrationTest.java
@@ -3,9 +3,10 @@ package com.appsdeveloperblog.tutorials.junit.ui.controllers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 //@TestPropertySource(locations = "/application-test.properties",
 //        properties = "server.port=8081")
 //@TestPropertySource(locations = "/application-test.properties")
@@ -14,8 +15,12 @@ public class UsersControllerIntegrationTest {
     @Value("${server.port}")
     private int serverPort;
 
+    @LocalServerPort
+    private int localServerPort;
+
     @Test
     void contextLoads() {
         System.out.println("server.port=" + serverPort);
+        System.out.println("local server port = " + localServerPort);
     }
 }
