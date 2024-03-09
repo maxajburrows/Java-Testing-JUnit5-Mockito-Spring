468a26515af078ef204d596447b8b37baadbabcb
Max Burrows <maxajburrows@gmail.com>
2024-03-09 16:12:51 +0100

    Test entity with invalid details

--- UsersServiceSpringBoot/src/test/java/com/appsdeveloperblog/tutorials/junit/io/UserEntityIntegrationTest.java
+++ UsersServiceSpringBoot/src/test/java/com/appsdeveloperblog/tutorials/junit/io/UserEntityIntegrationTest.java
@@ -1,31 +1,35 @@
 package com.appsdeveloperblog.tutorials.junit.io;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
+import javax.persistence.PersistenceException;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 public class UserEntityIntegrationTest {
 
     @Autowired
     private TestEntityManager testEntityManager;
+    UserEntity userEntity;
 
-    @Test
-    void testUserEntity_whenValidUserDetailsProvided_shouldReturnStoredUserDetails() {
-        // Arrange
-        UserEntity userEntity = new UserEntity();
+    @BeforeEach
+    void setUp() {
+        userEntity = new UserEntity();
         userEntity.setUserId(UUID.randomUUID().toString());
         userEntity.setFirstName("Max");
         userEntity.setLastName("Burrows");
         userEntity.setEmail("test@test.com");
         userEntity.setEncryptedPassword("12345678");
+    }
 
+    @Test
+    void testUserEntity_whenValidUserDetailsProvided_shouldReturnStoredUserDetails() {
         // Act
         UserEntity storedUserEntity = testEntityManager.persistAndFlush(userEntity);
 
@@ -36,5 +40,16 @@ public class UserEntityIntegrationTest {
         assertEquals(userEntity.getLastName(), storedUserEntity.getLastName());
         assertEquals(userEntity.getEmail(), storedUserEntity.getEmail());
         assertEquals(userEntity.getEncryptedPassword(), storedUserEntity.getEncryptedPassword());
+    }
+
+    @Test
+    void testUserEntity_whenFirstNameIsTooLong_shouldThrowException() {
+        // Arrange
+        userEntity.setFirstName("m".repeat(51));
+
+        // Assert & Act
+        assertThrows(PersistenceException.class, ()->{
+            testEntityManager.persistAndFlush(userEntity);
+        }, "Was expecting a PersistenceException to be thrown.");
     }
 }
